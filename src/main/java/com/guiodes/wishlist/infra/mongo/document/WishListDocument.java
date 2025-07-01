429c51e5decb302532b34361a45c2fbe937f5639
Guilherme <guilherme.montnezes@gmail.com>
2025-07-01 14:50:07 -0300

    UserId index, readme and cucumber report

--- src/main/java/com/guiodes/wishlist/infra/mongo/document/WishListDocument.java
+++ src/main/java/com/guiodes/wishlist/infra/mongo/document/WishListDocument.java
@@ -2,13 +2,18 @@ package com.guiodes.wishlist.infra.mongo.document;
 
 import com.guiodes.wishlist.domain.model.WishListModel;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 import java.util.UUID;
 
 @Document("WishList")
-public record WishListDocument(@Id UUID id, UUID userId, List<UUID> productList) {
+public record WishListDocument(
+        @Id UUID id,
+        @Indexed(unique = true) UUID userId,
+        List<UUID> productList
+) {
 
     public WishListDocument(WishListModel wishListModel) {
         this(wishListModel.id(), wishListModel.userId(), wishListModel.productList());
